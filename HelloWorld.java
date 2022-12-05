44706127b17259082b6a60dcc9d64b48bce4573e
shruthi496 <59406518+shruthi496@users.noreply.github.com>
2022-12-05 19:34:31 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello,this is jenkinssssssssssss"); 
+        System.out.println("Hello,this is jenkinsss"); 
     }
 }
